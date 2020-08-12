d5e0708088c6e1fdd9a57f98196c766f280ca6b3
cadenabahos <cadenahos@gmail.com>
2020-08-12 04:57:34 -0500

    feat: React-create-app en Heroku

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import BotonMensaje from '../BotonMensaje';
-import {Paper, Grid } from '@material-ui/core';
+import { Paper, Grid } from '@material-ui/core';
 import  useStyles from '../MuiStyle';
 
 export default function App() {
